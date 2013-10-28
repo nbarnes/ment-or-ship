3960eac0e7da74797db521bdee9b40ce501d36db
NBarnes <nbarnes@gmail.com>
2013-10-28 16:39:15 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentOrShip::Application.config.secret_token = 'ce5850c67d47705e66f671eef9497ecbd8031ff52bc360fb22aa5298b12b5c3f9af913ceb90878044bac01a4c14263ae5aa3f519d403888c3097aab5f730afa7'
